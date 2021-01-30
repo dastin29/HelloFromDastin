fa5fd8c6ee59eef45f6489a7ee8947c463f6db6b
domik <u209453172@byui.edu>
2021-01-30 02:47:46 -0700

    I updated with a new message from Dustin!

--- src/lodge/Main.java
+++ src/lodge/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("This is me telling you HELLO FROM DASTIN, not DUSTIN!");
+
+	    System.out.println("Hello Dastin! My name is Dustin! Similar names, we have!");
     }
 }
